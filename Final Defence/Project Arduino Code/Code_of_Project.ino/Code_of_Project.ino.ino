3a7e2b50c15b859bbba4aafe40a614e31664e5ac
shahriar hasan <shahriar.hasan.1523@gmail.com>
2023-08-30 01:58:59 +0600

    Added Readme and licence

--- Final Defence/Project Arduino Code/Code_of_Project.ino/Code_of_Project.ino.ino
+++ Final Defence/Project Arduino Code/Code_of_Project.ino/Code_of_Project.ino.ino
@@ -1,18 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#include <NewPing.h>  // Only if using an ultrasonic water level sensor
+#include <NewPing.h> // Only if using an ultrasonic water level sensor
 
 // Replace with your WiFi credentials
-char auth[] = "cmLtwz07mRPazy6h5X2fjBk_XjUeq6Jg";
-char ssid[] = "Project Moon";
-char pass[] = "18701012";
+char auth[] = "Your Blynk App Credintial";
+char ssid[] = "Your WiFi Name";
+char pass[] = "Your WiFi Password";
 
 // Pins
-const int waterLevelPin = D1;    // Pin for water level sensor
-const int soilMoisturePin = A0;  // Pin for soil moisture sensor
-const int rainSensorPin = D2;    // Pin for rain sensor
-const int waterPump = D3;        // pin for Motor
-
+const int waterLevelPin = D1;   // Pin for water level sensor
+const int soilMoisturePin = A0; // Pin for soil moisture sensor
+const int rainSensorPin = D2;   // Pin for rain sensor
+const int waterPump = D3;       // pin for Motor
 
 // Blynk virtual pins
 #define SOIL_MOISTURE_VIRTUAL V0
@@ -20,8 +19,8 @@ const int waterPump = D3;        // pin for Motor
 #define SOIL_MOISTURE_STATUS_VIRTUAL V2
 #define WATER_PUMP_VIRTUAL V3
 
-#define BLYNK_TEMPLATE_ID "TMPL65ba0tg7O"
-#define BLYNK_TEMPLATE_NAME "Tea Garden Sprinkler System"
+#define BLYNK_TEMPLATE_ID "Your Blynk Template ID"
+#define BLYNK_TEMPLATE_NAME "Your Blynk Template NAME"
 
 // Variables
 int waterLevel;
@@ -29,23 +28,21 @@ int soilMoisture;
 int rainSensorState;
 bool blynkServerConnected = false;
 int motorState;
-// (Optional) Initialize NewPing library for ultrasonic sensor
-// #define MAX_DISTANCE 200  // Change this value based on your sensor's capabilities
-// NewPing sonar(waterLevelPin, waterLevelPin, MAX_DISTANCE);
-
-
 
 // Function to read rain sensor state (1 if wet, 0 if dry)
-int getRainSensorState() {
+int getRainSensorState()
+{
   return digitalRead(rainSensorPin);
 }
 
 // Function to read soil moisture sensor value (0 to 1023)
-int getSoilMoisture() {
+int getSoilMoisture()
+{
   return analogRead(soilMoisturePin);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   pinMode(waterPump, OUTPUT);
@@ -56,25 +53,30 @@ void setup() {
   // Connect to Wi-Fi
   WiFi.begin(ssid, pass);
   Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("\nConnected to WiFi");
 
+  // Initialising Blynk
   Blynk.begin(auth, ssid, pass);
 
-  // (Optional) Set the frequency for reading sensors
-  Blynk.virtualWrite(SOIL_MOISTURE_VIRTUAL, 100);   // 5 seconds
-  Blynk.virtualWrite(WEATHER_STATUS_VIRTUAL, 100);  // 5 seconds
-  Blynk.virtualWrite(WATER_PUMP_VIRTUAL, 100);      // 5 seconds
+  // (Optional) Set the frequency for reading sensors // milisecond time to read
+  Blynk.virtualWrite(SOIL_MOISTURE_VIRTUAL, 1000);  // 1 seconds
+  Blynk.virtualWrite(WEATHER_STATUS_VIRTUAL, 1000); // 1 seconds
+  Blynk.virtualWrite(WATER_PUMP_VIRTUAL, 100);      // 0.1 seconds
 }
 
-void loop() {
-  // connecting to blynk server
-  if (!blynkServerConnected) {
+void loop()
+{
+  // connecting to Blynk server
+  if (!blynkServerConnected)
+  {
     Serial.print("\n\n\nConnecting to Blynk Server");
-    while (!Blynk.connected()) {
+    while (!Blynk.connected())
+    {
       delay(500);
       Serial.print(".");
     }
@@ -82,70 +84,66 @@ void loop() {
     blynkServerConnected = true;
   }
 
-
-
-
-
-
-
-  //soil moisture display section
+  // soil moisture display section
   soilMoisture = getSoilMoisture();
-  soilMoisture = (map(soilMoisture, 0, 1024, 0, 100) - 100) * -1;
+  soilMoisture = (map(soilMoisture, 0, 1024, 0, 100) - 100) * -1; // converting value of range 0 - 1023 to range 0 - 100
   Blynk.virtualWrite(SOIL_MOISTURE_VIRTUAL, soilMoisture);
   Serial.print("Soil Moisture Data is = ");
   Serial.println(soilMoisture);
-  if (soilMoisture < 20) {
+  if (soilMoisture < 20)
+  {
     Blynk.virtualWrite(SOIL_MOISTURE_STATUS_VIRTUAL, "Soil is too DRY - Need Water");
-  } else if (soilMoisture < 50 && soilMoisture >= 20) {
+  }
+  else if (soilMoisture < 50 && soilMoisture >= 20)
+  {
     Blynk.virtualWrite(SOIL_MOISTURE_STATUS_VIRTUAL, "Soil is PERFECT");
-  } else {
+  }
+  else
+  {
     Blynk.virtualWrite(SOIL_MOISTURE_STATUS_VIRTUAL, "Soil is too WET");
   }
 
-
-
-
-
-
-  //Weather Status display section
+  // Weather Status display section
   rainSensorState = getRainSensorState();
   Serial.print("Wether Status is = ");
-  if (rainSensorState == 0) {
+  if (rainSensorState == 0)
+  {
     Serial.println("Raining");
     Blynk.virtualWrite(WEATHER_STATUS_VIRTUAL, "Raining");
-  } else {
+  }
+  else
+  {
     Serial.println("Sunny");
     Blynk.virtualWrite(WEATHER_STATUS_VIRTUAL, "Not Raining");
   }
-
-
-
-
 
   // logic Section of Motor ON or OFF
 
   Serial.print("Motor state ");
   Serial.println(motorState);
-  if (rainSensorState != 0 && soilMoisture <= 20 && motorState == 1) {
+  if (rainSensorState != 0 && soilMoisture <= 20 && motorState == 1)
+  {
     Blynk.virtualWrite(WATER_PUMP_VIRTUAL, 1);
     digitalWrite(waterPump, LOW);
     Serial.println("Motor is ON\n\n");
-  } else {
+  }
+  else
+  {
     Blynk.virtualWrite(WATER_PUMP_VIRTUAL, 0);
     digitalWrite(waterPump, HIGH);
     Serial.println("Motor is OFF\n\n");
   }
 
-
-
   Blynk.run();
-  delay(1000);
+  delay(1000); // the loop will run after every 1 second
 }
 
-BLYNK_CONNECTED() {
+BLYNK_CONNECTED()
+{
   // Request the latest state from the server
   Blynk.syncVirtual(WATER_PUMP_VIRTUAL);
 }
-BLYNK_WRITE(V3) {
-  motorState = param.asInt();  // Get the state of the button (0 or 1)
+BLYNK_WRITE(V3)
+{
+  motorState = param.asInt(); // Get the state of the button (0 or 1)
 }
---
+++ README.md
@@ -0,0 +1,68 @@
+# IoT based Automated Irrigation System & Mobile App for Tea Garden
+
+![IoT based Automated Irrigation System & Mobile App for Tea Garden](./cover.png)
+
+## B.Sc. Engineering Thesis
+
+### Course Code: CSE 800
+
+#### Submitted to:
+
+Department of Computer Science & Engineering  
+University of Chittagong
+
+---
+
+## Project Overview
+
+The **IoT based Automated Irrigation System & Mobile App for Tea Garden** is a B.Sc. Engineering thesis project carried out in partial fulfillment of the requirements for the degree of B.Sc. (Engineering) in Computer Science and Engineering at the University of Chittagong. This project aims to develop an automated irrigation system for tea gardens using Internet of Things (IoT) technology along with a mobile app interface.
+
+---
+
+## Project Features
+
+- **Automated Irrigation:** The system utilizes various sensors, including soil moisture and rain sensors, to determine the optimal irrigation needs for the tea garden.
+- **Mobile App Interface:** A mobile app is developed to provide real-time monitoring and control of the irrigation system remotely. The Pumping motor can also be manually turn OFF or ON by the Mobile App.
+- **Weather Status:** The system can detect rain and sunny conditions using the rain sensor, providing accurate weather data to assist in irrigation decisions.
+- **Soil Moisture Monitoring:** Soil moisture sensors measure the soil's moisture level, ensuring that plants receive the appropriate amount of water.
+- **Water Pump Control:** The system automatically controls the water pump based on the weather conditions and soil moisture levels.
+
+---
+
+## Installation and Setup
+
+1. Clone this repository to your local machine.
+2. Install the required libraries using the Arduino IDE's Library Manager:
+   - ESP8266WiFi
+   - BlynkSimpleEsp8266
+3. Open the Arduino IDE and load the **Project Arduino Code** in Final Defence Section.
+4. Replace the placeholder values in the code with your WiFi credentials and Blynk authentication token.
+5. Upload the code to your ESP8266 device using cable connector.
+6. Install the Blynk app on your mobile device and create a project using the provided template ID and name.
+   ![Setup](./Final%20Defence/Assets/Overview%201.png)
+
+---
+
+## Usage
+
+1. Power up your ESP8266 device.
+2. Turn on the Wifi Router or Mobile hotspot where you want to connect.
+3. Launch the Blynk app on your mobile device and connect to the project.
+4. Monitor the soil moisture and weather status in real-time.
+5. Control the water pump manually or let the system automate the irrigation based on sensor readings.
+   ![Usage](./Final%20Defence/Assets/Overview%202.png)
+
+---
+
+## Contact
+
+For questions or inquiries, please contact:
+
+- Shariar Hasan
+- Email: shahriar.hasan.1523@gmail.com
+
+---
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
